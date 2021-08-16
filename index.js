90f1c94f9aeb48d57c60657219a04674305d7ca9
Anders Rune Jensen <arj03@protonmail.ch>
2021-08-16 14:50:30 +0200

    meta feeds working

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const ssbSingleton = require('ssb-browser-core/ssb-singleton')
 
 function ssbReady(sbot) {
   console.log("got sbot", sbot)
+  sbot.net.metafeeds.create((err, metafeed) => {
+    console.log("I have a meta feed", metafeed)
+  })
 }
 
 function extraModules(secretStack) {
-  // add extra modules here
-  return secretStack
+  return secretStack.use(require("ssb-meta-feeds"))
 }
 
 // in case you want to add or overwrite something from here
@@ -19,7 +21,7 @@ ssbSingleton.setup("/.ssb-8k", config, extraModules, () => {})
 ssbSingleton.getSSBEventually(
   -1,
   () => { return true },
-  (SSB) => { return SSB },
+  (SSB) => { return SSB && SSB.net },
   (err, SSB) => {
     if (err) console.error(err)
     else ssbReady(SSB)
